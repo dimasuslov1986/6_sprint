f2e4348e410beae440cd9827a71e7d6eb9f06b9a
dimasuslov1986 <dima.suslov.1986@gmail.com>
2025-05-15 23:26:52 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
 
+	// создать логгер
+	// New(out io.Writer, prefix string, flag int) *Logger
+	logger := log.New(os.Stdout, "INFO", log.LstdFlags)
+	// создать сервер с помощью вашей функции из пакета server
+	serv := server.New(logger)
+	// запустить его
+	if err := serv.Serv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,74 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func MainHandle(res http.ResponseWriter, req *http.Request) {
+	curDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := os.ReadFile(filepath.Join(curDir, "index.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
+	// res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// res.WriteHeader(http.StatusOK)
+	// res.Write(?)
+}
+
+func UploadHandle(res http.ResponseWriter, req *http.Request) {
+
+	// Парсить html-форму из файла index.html.
+	req.ParseMultipartForm(10 << 20)
+
+	// Получить файл из формы (не забудьте его закрыть).
+	file, handler, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(res, "Ошибка при получении файла", http.StatusInternalServerError)
+		return
+	}
+	// закрываем файл
+	defer file.Close()
+
+	// Прочитать данные из файла.
+	body, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Ошибка чтения:", http.StatusInternalServerError)
+		return
+	}
+	// Передать эти данные в функцию автоопределения из пакета service, которую вы создали, чтобы получить переконвертируемую строку.
+	result, err := service.Convert(string(body))
+	if err != nil {
+		fmt.Println("Ошибка конвертации:", http.StatusInternalServerError)
+		return
+	}
+
+	// Создать локальный файл. Эта операция обычно небезопасна и так делать не рекомендуется, но в рамках нашего задания хотелось бы более наглядного результата, поэтому мы решились на этот шаг, ради видимого результата. А вообще, обычно используют временные файлы.
+	nameNewFile := time.Now().UTC().String()
+	// получаем текущую директорию
+	curDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(http.StatusInternalServerError)
+	}
+	ext := filepath.Ext(handler.Filename)
+	newFile := filepath.Join(curDir, nameNewFile, ext)
+
+	// Записать в локальный файл результат конвертации строки. Для генерации имени файла вы можете использовать время с помощью time.Now().UTC().String(). Чтобы получить расширения файла, используйте filepath.Ext().
+	err = os.WriteFile(newFile, []byte(result), 0755)
+	if err != nil {
+		log.Fatal(http.StatusInternalServerError)
+	}
+	// Вернуть результат конвертации строки.
+	res.Write([]byte(req.FormValue(result)))
+}
